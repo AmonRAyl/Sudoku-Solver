202c4c3cba5a93c4aa70dc0fb011065acb1c39dc
AmonRA <80157145+AmonRAyl@users.noreply.github.com>
2023-08-21 18:02:15 +0200

    Create sudokufinder.js

---
+++ sudokufinder.js
@@ -0,0 +1,50 @@
+const apiUrl = 'https://sudoku-api.vercel.app/api/dosuku';
+const fs = require('fs');
+
+function fetchSudoku() {
+  fetch(apiUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('HTTP request failed: ' + response.status);
+      }
+    })
+    .then(data => {
+      let aux=data["newboard"]["grids"][0];
+      if (aux["difficulty"] === 'Easy' || aux["difficulty"] === 'Medium') {
+          let coordinates = [];
+            for (let i = 0; i < 9; i++) {
+                for (let j = 0; j < 9; j++){
+                    if (aux["value"][i][j] != 0){
+                        let string= "g["+i+"]["+j+"].setActualValue('"+aux["value"][i][j]+"');";
+                        coordinates.push(string);
+                    }
+                }
+            }
+
+          let newContent = `Problem:\n${matrixToString(aux["value"])}\nSolution:\n${matrixToString(aux["solution"])}\n\n${coordinates.join('\n')}`;
+          
+          function matrixToString(matrix) {
+            return matrix.map(row => row.join(' ')).join('\n');
+          }
+          
+          fs.appendFile('sudokus.txt', '\n\n' + newContent, (err) => {
+            if (err) {
+              console.error('Error appending to file:', err);
+            } else {
+              console.log('New data appended to output.txt');
+            }
+          });
+          
+      } else {
+        console.log('Retrying for an easy/medim difficulty Sudoku...');
+        fetchSudoku();
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+fetchSudoku();
