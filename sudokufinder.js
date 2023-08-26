38e83bf55e7125301300f90398ebd48247e79e5b
AmonRA <80157145+AmonRAyl@users.noreply.github.com>
2023-08-26 12:48:38 +0200

    All Difficulties allowed + new format

--- sudokufinder.js
+++ sudokufinder.js
@@ -12,35 +12,29 @@ function fetchSudoku() {
     })
     .then(data => {
       let aux=data["newboard"]["grids"][0];
-      if (aux["difficulty"] === 'Easy' || aux["difficulty"] === 'Medium') {
-          let coordinates = [];
-            for (let i = 0; i < 9; i++) {
-                for (let j = 0; j < 9; j++){
-                    if (aux["value"][i][j] != 0){
-                        let string= "g["+i+"]["+j+"].setActualValue('"+aux["value"][i][j]+"');";
-                        coordinates.push(string);
-                    }
-                }
-            }
-
-          let newContent = `Problem:\n${matrixToString(aux["value"])}\nSolution:\n${matrixToString(aux["solution"])}\n\n${coordinates.join('\n')}`;
-          
-          function matrixToString(matrix) {
-            return matrix.map(row => row.join(' ')).join('\n');
+        let coordinates = [];
+          for (let i = 0; i < 9; i++) {
+              for (let j = 0; j < 9; j++){
+                  if (aux["value"][i][j] != 0){
+                      let string= "g["+i+"]["+j+"].setActualValue('"+aux["value"][i][j]+"',0);";
+                      coordinates.push(string);
+                  }
+              }
           }
-          
-          fs.appendFile('sudokus.txt', '\n\n' + newContent, (err) => {
-            if (err) {
-              console.error('Error appending to file:', err);
-            } else {
-              console.log('New data appended to output.txt');
-            }
-          });
-          
-      } else {
-        console.log('Retrying for an easy/medim difficulty Sudoku...');
-        fetchSudoku();
-      }
+
+        let newContent = `Difficulty:\n${aux["difficulty"]}\nProblem:\n${matrixToString(aux["value"])}\nSolution:\n${matrixToString(aux["solution"])}\n\n${coordinates.join('\n')}`;
+        
+        function matrixToString(matrix) {
+          return matrix.map(row => row.join(' ')).join('\n');
+        }
+        
+        fs.appendFile('sudokus.txt', '\n\n' + newContent, (err) => {
+          if (err) {
+            console.error('Error appending to file:', err);
+          } else {
+            console.log('New data appended to output.txt');
+          }
+        });
     })
     .catch(error => {
       console.error('Error:', error);
